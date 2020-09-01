94e2f7b6dec90bbc114f526ad6fb70a31c2e0f99
Marisa You <marisa.you@gmail.com>
2020-09-01 19:57:26 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -8,6 +8,6 @@ def happy_birthday(birthday_kids)
   # add your code snippet here!
   
   birthday_kids.each do |key, value|
-    puts "#{key}: #{value}"
+    puts "Happy Birthday #{key}! You are now #{value} years old!"
   end
 end
